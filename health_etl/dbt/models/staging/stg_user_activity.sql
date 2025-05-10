d20e749902860fe23bc689bd1185b9bdfb262449
Alexander Antonison <adantonison@gmail.com>
2025-05-10 21:44:09 +0100

    Update user activity to account for updated app_result

--- health_etl/dbt/models/staging/stg_user_activity.sql
+++ health_etl/dbt/models/staging/stg_user_activity.sql
@@ -6,7 +6,8 @@ date_time_app_data AS (
         content_slug,
         created_time,
         modified_time,
-        cast(value as varchar) as value
+        cast(value as varchar) as value,
+        rank() over(partition by app_result_id order by modified_time desc) AS record_rank
     FROM {{ source('app_results', 'datetime_app_results') }}
 ),
 
@@ -16,7 +17,8 @@ integer_app_data AS (
         content_slug,
         created_time,
         modified_time,
-        cast(value as varchar) as value
+        cast(value as varchar) as value,
+        rank() over(partition by app_result_id order by modified_time desc) AS record_rank
     FROM {{ source('app_results', 'integer_app_results') }}
 ),
 
@@ -26,18 +28,22 @@ range_app_results AS (
         content_slug,
         created_time,
         modified_time,
-        cast(from_value AS varchar) || ' - ' || cast(to_value AS varchar) AS value
+        cast(from_value AS varchar) || ' - ' || cast(to_value AS varchar) AS value,
+        rank() over(partition by app_result_id order by modified_time desc) AS record_rank
     FROM {{ source('app_results', 'range_app_results') }}
 )
 
 SELECT
     *
 FROM date_time_app_data
+WHERE record_rank = 1
 UNION ALL
 SELECT
     *
 FROM integer_app_data
+WHERE record_rank = 1
 UNION ALL
 SELECT
     *
 FROM range_app_results
+WHERE record_rank = 1
