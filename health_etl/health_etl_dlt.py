dba52b8f2bfc07c87ccfe5224baee280af4a60f2
Alexander Antonison <adantonison@gmail.com>
2025-05-18 15:39:51 +0100

    Add in dlt loading into duckdb

---
+++ health_etl/health_etl_dlt.py
@@ -0,0 +1,147 @@
+import dlt
+from datetime import datetime
+import duckdb
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def get_duckdb_conn():
+    return duckdb.connect("healthetl_pipeline.duckdb")
+
+def get_last_modified_time(table_name):
+    """Get the last modified time from the DuckDB destination"""
+
+    duckdb_conn = get_duckdb_conn()
+    try:
+        result = duckdb_conn.execute(f"""
+            SELECT MAX(modified_time) as last_modified
+            FROM {table_name}
+        """).fetchone()
+        return result[0] if result and result[0] else datetime.min
+    except Exception:
+        return datetime.min
+
+@dlt.resource(
+        table_name="stg_integer_app_results",
+        write_disposition="merge",
+        primary_key="app_result_id"
+)
+def load_integer_app_results(pg_conn):
+
+    last_modified_time = get_last_modified_time("stg_integer_app_results")
+
+    with pg_conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute(
+            """
+            SELECT 	
+                iar.app_result_id,
+                ar.content_slug,
+                ar.created_time,
+                ar.modified_time,
+                iar.value
+            FROM public.integer_app_results iar
+            JOIN public.app_results ar ON ar.id = iar.app_result_id
+            WHERE ar.polymorphic_type = 'IntegerAppResult'
+            AND ar.modified_time > %s
+            """,
+            (last_modified_time,),
+        )
+
+        results = cursor.fetchall()
+
+    data = [dict(row) for row in results]
+
+    yield data
+
+
+@dlt.resource(
+        table_name="stg_date_time_app_results",
+        write_disposition="merge",
+        primary_key="app_result_id"
+)
+def load_date_time_app_results(pg_conn):
+
+    last_modified_time = get_last_modified_time("stg_date_time_app_results")
+
+    pg_conn = psycopg2.connect(
+        host=dlt.secrets["postgres"]["host"],
+        port=dlt.secrets["postgres"]["port"],
+        database=dlt.secrets["postgres"]["database"],
+        user=dlt.secrets["postgres"]["username"],
+        password=dlt.secrets["postgres"]["password"],
+    )
+
+    with pg_conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute(
+            """
+            SELECT 	
+                dar.app_result_id,
+                ar.content_slug,
+                ar.created_time,
+                ar.modified_time,
+                dar.value
+            FROM public.datetime_app_results dar 
+            JOIN public.app_results ar ON ar.id = dar.app_result_id
+            WHERE ar.polymorphic_type = 'DateTimeAppResult'
+            AND ar.modified_time > %s
+            """,
+            (last_modified_time,),
+        )
+
+        results = cursor.fetchall()
+
+    data = [dict(row) for row in results]
+
+    yield data
+
+@dlt.resource(
+        table_name="stg_range_app_results",
+        write_disposition="merge",
+        primary_key="app_result_id"
+)
+def load_range_app_results(pg_conn):
+
+    last_modified_time = get_last_modified_time("stg_range_app_results")
+
+    with pg_conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute(
+            """
+            SELECT 	
+                rar.app_result_id,
+                ar.content_slug,
+                ar.created_time,
+                ar.modified_time,
+                rar.from_value,
+                rar.to_value
+            FROM public.range_app_results rar 
+            JOIN public.app_results ar ON ar.id = rar.app_result_id
+            WHERE ar.polymorphic_type = 'RangeAppResult'
+            AND ar.modified_time > %s
+            """,
+            (last_modified_time,),
+        )
+
+        results = cursor.fetchall()
+
+    data = [dict(row) for row in results]
+
+    yield data
+
+pipeline = dlt.pipeline(
+    pipeline_name="healthetl_pipeline",
+    destination="duckdb",
+    dataset_name="health_etl"
+)
+
+pg_conn = psycopg2.connect(
+    host=dlt.secrets["postgres"]["host"],
+    port=dlt.secrets["postgres"]["port"],
+    database=dlt.secrets["postgres"]["database"],
+    user=dlt.secrets["postgres"]["username"],
+    password=dlt.secrets["postgres"]["password"],
+)
+
+load_info = pipeline.run(load_integer_app_results(pg_conn))
+load_info = pipeline.run(load_date_time_app_results(pg_conn))
+load_info = pipeline.run(load_range_app_results(pg_conn))
+
+print(load_info)
