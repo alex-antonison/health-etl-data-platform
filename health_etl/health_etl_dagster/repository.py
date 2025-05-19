675cdaf4f28d48565956dce5e3777c2137a70995
Alexander Antonison <adantonison@gmail.com>
2025-05-19 10:20:39 +0100

    Update dagster repository

--- health_etl/health_etl_dagster/repository.py
+++ health_etl/health_etl_dagster/repository.py
@@ -1,12 +1,29 @@
 from dagster import Definitions
 from .assets import run_load_date_time_app_results, run_load_integer_app_results, run_load_range_app_results
+from pathlib import Path
+from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
+import dagster as dg
 
-# from .jobs import dlt_job
+RELATIVE_PATH_TO_MY_DBT_PROJECT = "../dbt"
+
+dbt_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", RELATIVE_PATH_TO_MY_DBT_PROJECT).resolve(),
+)
+dbt_project.prepare_if_dev()
+
+@dbt_assets(manifest=dbt_project.manifest_path)
+def dbt_assets(context: dg.AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
+
 
 defs = Definitions(
     assets=[
         run_load_date_time_app_results,
         run_load_integer_app_results,
-        run_load_range_app_results
+        run_load_range_app_results,
+        dbt_assets,
     ],
+    resources={
+        "dbt": DbtCliResource(project_dir=dbt_project),
+    },
 )
